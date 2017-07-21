efcf91d79b326bc61644b02e0ace3e188a3d5719
Victor Christensen <victor.christensen@cox.net>
2017-07-21 04:09:24 +0000

    firehose track 7, solo GenET project landing page

---
+++ app/controllers/landing_pages_controller.rb
@@ -0,0 +1,8 @@
+# firehose track 7, step 3 - file created 20 jul 17 for solo GenET project landing page
+
+class LandingPagesController < ApplicationController
+    
+  def index
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+# firehose track 7, step 3 - file created 20 jul 17 for solo GenET project landing page
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+# next line added 20 jul 17 for solo GenET project landing page
+  root 'landing_pages#index'    # hooks up landing page to index action - app/views/index.html.erb
+  
 end
