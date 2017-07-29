244a8b782e5342757ffff2a6555c32028050537d
Victor Christensen <victor.christensen@cox.net>
2017-07-29 04:36:40 +0000

    firehose track 7, step 15 solo GenET project making submit button create person

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -5,4 +5,17 @@ class PeopleController < ApplicationController
     @person = Person.new
   end
   
+# create method added 28 jul 17 for making submit form create new person (step 15)
+  def create
+    Person.create(person_params)
+    redirect_to new_person_path
+  end
+
+  private
+
+# person_params method added 28 jul 17 for making submit form create new person (step 15)
+  def person_params
+    params.require(:person).permit(:first_name, :middle_name, :last_name, :sex, :birth_date, :death_date, :father_id, :mother_id, :current_spouse, :notes)
+  end
+  
 end
